0331176147780bfc293fcf50af8b7106c87b3326
Estefany Rueda Romero <47457951+StefaRueRome@users.noreply.github.com>
2020-04-09 21:37:58 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaboratorioEnsuringTruth_Pedro_Estefany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
