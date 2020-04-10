fc1f85c2294276d0dbf044e0fe14490f067d3eb7
Pedro Felipe Gomez Bonilla <pedro.gomez.2019@upb.edu.co>
2020-04-09 21:43:16 -0500

    Cases update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace LaboratorioEnsuringTruth_Pedro_Estefany
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String entrada = "", salida = "", casosString = "";
+            int casos = 0;
+            casosString = Console.ReadLine();
+            casos = Int32.Parse(casosString);
+            //Console.WriteLine(casos);
         }
     }
 }
